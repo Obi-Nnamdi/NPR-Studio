7c298277455d7a4db67563056a5bc2f2474cd13d
Nnamdi Obi <nnamdiobi35@gmail.com>
2023-12-12 20:36:23 -0500

    Made SunNode toggleable between point and sun lights.

--- assignment_code/assignment6/SunNode.cpp
+++ assignment_code/assignment6/SunNode.cpp
@@ -6,10 +6,33 @@
 
 namespace GLOO {
 SunNode::SunNode() : time_elapsed_(0.0f) {
-  light_ = std::make_shared<DirectionalLight>();
-  light_->SetDiffuseColor(glm::vec3(0.8f, 0.8f, 0.8f));
-  light_->SetSpecularColor(glm::vec3(1.0f, 1.0f, 1.0f));
-  CreateComponent<LightComponent>(light_);
+  // Initialize both point and directional light
+  point_light_ = std::make_shared<PointLight>();
+  point_light_->SetDiffuseColor(glm::vec3(0.8f, 0.8f, 0.8f));
+  point_light_->SetSpecularColor(glm::vec3(1.0f, 1.0f, 1.0f));
+  point_light_->SetAttenuation(glm::vec3(0.15));
+
+  directional_light_ = std::make_shared<DirectionalLight>();
+  directional_light_->SetDiffuseColor(glm::vec3(0.8f, 0.8f, 0.8f));
+  directional_light_->SetSpecularColor(glm::vec3(1.0f, 1.0f, 1.0f));
+  CreateComponent<LightComponent>(directional_light_);
+  activated_light_ = LightType::Directional;
+}
+
+void SunNode::UpdatePosition(const glm::vec3& position) { GetTransform().SetPosition(position); }
+
+void SunNode::ToggleLight() {
+  RemoveComponent<LightComponent>();
+  // Toggle activated light variable
+  activated_light_ =
+      activated_light_ == LightType::Directional ? LightType::Point : LightType::Directional;
+
+  // Populate LightComponent with other light
+  if (activated_light_ == LightType::Directional) {
+    CreateComponent<LightComponent>(directional_light_);
+  } else {
+    CreateComponent<LightComponent>(point_light_);
+  }
 }
 
 void SunNode::Update(double delta_time) {
@@ -19,12 +42,13 @@ void SunNode::Update(double delta_time) {
   light_dir = glm::normalize(light_dir);
   glm::vec3 eye = 20.0f * light_dir;
   UpdateSun(eye, -light_dir);
+  UpdatePosition(2.f * light_dir);  // move point light to same "direction" sun light is in
 }
 
 void SunNode::UpdateSun(const glm::vec3& eye, const glm::vec3& direction) {
-  light_->SetDirection(glm::normalize(direction));
-  auto up_dir = glm::normalize(
-      glm::cross(light_->GetDirection(), glm::vec3(0.0f, 0.0f, 1.0f)));
+  directional_light_->SetDirection(glm::normalize(direction));
+  auto up_dir =
+      glm::normalize(glm::cross(directional_light_->GetDirection(), glm::vec3(0.0f, 0.0f, 1.0f)));
   auto sun_view = glm::lookAt(eye, glm::vec3(0.0f), up_dir);
 
   auto sun_to_world_mat = glm::inverse(sun_view);
--- assignment_code/assignment6/SunNode.hpp
+++ assignment_code/assignment6/SunNode.hpp
@@ -2,8 +2,8 @@
 #define SUN_NODE_H_
 
 #include "gloo/SceneNode.hpp"
-
 #include "gloo/lights/DirectionalLight.hpp"
+#include "gloo/lights/PointLight.hpp"
 
 namespace GLOO {
 class SunNode : public SceneNode {
@@ -11,11 +11,15 @@ class SunNode : public SceneNode {
   SunNode();
 
   void Update(double delta_time) override;
+  void ToggleLight();  // switches between directional "full" light and point light
 
  private:
   void UpdateSun(const glm::vec3& eye, const glm::vec3& direction);
+  void UpdatePosition(const glm::vec3& position);
 
-  std::shared_ptr<DirectionalLight> light_;
+  std::shared_ptr<DirectionalLight> directional_light_;
+  std::shared_ptr<PointLight> point_light_;
+  LightType activated_light_;
   double time_elapsed_;
 };
 }  // namespace GLOO
