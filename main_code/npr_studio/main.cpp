5d0a08f836e4810b2691444c3962a05613a109c7
Nnamdi Obi <nnamdiobi35@gmail.com>
2024-01-02 21:32:18 -0500

    Changed Project Window name.

--- main_code/npr_studio/main.cpp
+++ main_code/npr_studio/main.cpp
@@ -9,8 +9,7 @@ int main(int argc, char** argv) {
   // Populate the model to render with if it's specified
   std::string filename = argc >= 2 ? std::string(argv[1]) : "";
 
-  auto app = make_unique<ToonViewerApp>("Mini Non-Photorealistic Renderer", glm::ivec2(1440, 900),
-                                        filename);
+  auto app = make_unique<ToonViewerApp>("NPR Studio", glm::ivec2(1440, 900), filename);
 
   app->SetupScene();
 
